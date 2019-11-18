d5d7a3bedc3140416d73d9930fbc3e5ccb596cfd
AMINUL-IT-SOLUTIONS <AMINUL-IT-SOLUTIONS@DESKTOP-E2NQQN2>
2019-11-17 19:15:20 -0800

    7th commit

---
+++ src/while_loop/While_loop.java
@@ -0,0 +1,16 @@
+
+package while_loop;
+
+
+public class While_loop {
+
+    
+    public static void main(String[] args) {
+       int count=1;
+       while(count<=10){
+        System.out.println(count);
+       count++;
+    }
+    
+}
+}
